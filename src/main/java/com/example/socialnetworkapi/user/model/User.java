2bb5d68eeb2cde6aac2ac495930e4e59d201f1c0
comertbaldemir <comertbaldemir@gmail.com>
2024-12-16 18:12:45 +0300

    Create User model and dao for Neo4j integration

--- src/main/java/com/example/socialnetworkapi/user/model/User.java
+++ src/main/java/com/example/socialnetworkapi/user/model/User.java
@@ -2,18 +2,42 @@ package com.example.socialnetworkapi.user.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "user")
+@Node("user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username")
+    @Property(name = "username")
     private String username;
 
-    @Column(name = "password")
+    @Property(name = "password")
     private String password;
+
+    @Property(name = "email")
+    private String email;
+
+    @Property(name = "fullName")
+    private String fullName;
+
+    @Property(name = "bio")
+    private String bio;
+
+    @Property("profilePictureUrl")
+    private String profilePictureUrl;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/example/socialnetworkapi/user/repository/UserRepository.java
+++ src/main/java/com/example/socialnetworkapi/user/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.example.socialnetworkapi.user.repository;
 
 import com.example.socialnetworkapi.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends Neo4jRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
